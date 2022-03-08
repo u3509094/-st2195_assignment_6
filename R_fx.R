edb05aad4e9844a894f99782469c2bf01ac12aab
Lee Hin Sing Raymond <u3509094@connect.hku.hk>
2022-03-08 22:54:39 +0800

    Creating new columns: fx_diff, fx_diff(percentage), good_news, bad_news

--- R_fx.R
+++ R_fx.R
@@ -6,7 +6,7 @@ fx <- fx[, c("period", "value")]
 speeches <- read.csv("speeches.csv", sep = "|", quote = "")
 speeches <- speeches[, c("date", "contents")]
 
-#Joining fx and speeches dataset
+#Joining fx and speeches dataframe
 library(dplyr)
 fx_speeches <- fx %>%
   left_join(speeches, by = c("period" = "date"))
@@ -34,3 +34,16 @@ library(tidyr)
 fx_speeches <- fx_speeches %>% 
   fill(value, .direction = "up")
 sum(is.na(fx$value))
+
+#Repeat lines 13-17 and lines 29-34 on fx dataset
+fx$period <- as.Date(fx$period)
+fx$value <- as.numeric(fx$value)
+fx <- fill(fx, value, .direction = "up")
+sum(is.na(fx$value))
+
+#Create new columns in the fx dataframe
+fx <- mutate(fx,
+             fx_diff = value - lead(value),
+             fx_per_diff = fx_diff / value * 100,
+             good_news = ifelse(fx_per_diff > 0.5, 1, 0),
+             bad_news = ifelse(fx_per_diff < -0.5, 1, 0))
